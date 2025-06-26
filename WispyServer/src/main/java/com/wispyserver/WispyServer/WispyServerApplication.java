b8ba4b4d6e72c59d2230303fddf4621f7925fb39
0gonge <Challengef0802@gmail.com>
2025-06-26 17:00:21 +0900

    CHORE: Project 초기 Setting

---
+++ WispyServer/src/main/java/com/wispyserver/WispyServer/WispyServerApplication.java
@@ -0,0 +1,13 @@
+package com.wispyserver.WispyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WispyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WispyServerApplication.class, args);
+	}
+
+}
---
+++ WispyServer/src/test/java/com/wispyserver/WispyServer/WispyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wispyserver.WispyServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WispyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
