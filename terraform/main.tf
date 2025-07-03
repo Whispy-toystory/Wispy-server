76beaa04b9e2ce95e5ebf4b70b0acad092bf129c
0gonge <Challengef0802@gmail.com>
2025-07-03 15:49:11 +0900

    FEAT: provider 중복 설정 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-# terraform/main.tf
-
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -28,7 +10,7 @@ resource "aws_s3_bucket" "glb_bucket" {
 
   tags = {
     Name        = "glb-file-storage"
-    Environment = "Dev"
+    Environment = var.environment
+    Project     = var.project_name
   }
 }
-
