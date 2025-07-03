2be29dce486a04dde90602c8c17564394cbf78cb
0gonge <Challengef0802@gmail.com>
2025-07-03 15:17:29 +0900

    FEAT: main.tf에 aws 기본 설정 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,26 @@
 # terraform/main.tf
 
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 resource "aws_s3_bucket" "glb_bucket" {
   bucket = var.bucket_name
 
