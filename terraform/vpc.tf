9fbf37c824f79e7e09421b454b43174c86538356
0gonge <Challengef0802@gmail.com>
2025-07-03 15:30:25 +0900

    FEAT: vpc.tf 구성

---
+++ terraform/vpc.tf
@@ -0,0 +1,37 @@
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  # EKS 클러스터용 태그
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment
+    Project = var.project_name
+  }
+}
